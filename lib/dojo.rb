91e19c91302cd6c8c37d8fd9783d993ba9b1290a
YaoShangTseng <yakushou730@gmail.com>
2016-08-05 22:40:01 +0800

    not finish #hire

--- lib/dojo.rb
+++ lib/dojo.rb
@@ -2,14 +2,19 @@ class Dojo
 
   include Enumerable
 
-  attr_accessor :stats
+  attr_accessor :stats, :team
 
   def initialize
     @stats = {raw: [100], groups: [10]}
+    @team = []
   end
 
   def discount(money, func)
     func.call(money)
+  end
+
+  def hire(*employee)
+    yield @team
   end
 
 end
