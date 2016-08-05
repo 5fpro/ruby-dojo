25e3cca0e6d4a94d341c34de3f25d9a0896951b5
YaoShangTseng <yakushou730@gmail.com>
2016-08-05 22:22:10 +0800

    not finish #stats

--- lib/dojo.rb
+++ lib/dojo.rb
@@ -1,5 +1,13 @@
 class Dojo
 
+  include Enumerable
+
+  attr_accessor :stats
+
+  def initialize
+    @stats = {raw: [100], groups: [10]}
+  end
+
   def discount(money, func)
     func.call(money)
   end
