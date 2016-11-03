ad14ec0fa03b752241ea1419a50d4c71fae2a6ef
ellenlee <leechinghui.tw@gmail.com>
2016-11-04 04:38:07 +0800

    pass discount and stats

--- lib/dojo.rb
+++ lib/dojo.rb
@@ -1,1 +1,21 @@
+class Dojo
+
+  def discount(price, func)
+    func.call(price)
+  end
+
+  def stats
+    raw = [10,20,30,40]
+    group = [[{:avg=>1}],[{:avg=>2}],[{:avg=>3}],[{:avg=>4}]]
+
+    {:raw => raw, :groups => group }
+  end
+
+  def hire
+
+  end
+
+
+end
+
 
