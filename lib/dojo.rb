dbc52d3aab94823a726cac6d39a10d078dc1d0da
Sam Lin <greenlin111@gmail.com>
2016-11-09 17:20:58 +0800

    add stats method

--- lib/dojo.rb
+++ lib/dojo.rb
@@ -1,7 +1,19 @@
 class Dojo 
-	
+
 	def discount(price,func)
 	  func.call(price)
+	end
+
+	def stats
+		raw =[]
+		groups = []
+		5.times do |i|
+			raw << i * 10
+			groups << [{ :avg => i}]
+		end
+
+		{:raw => raw, 
+		:groups => groups}
 	end
 
 end
